36b04efd979ae57c896d59b27fad12cdf5b9be79
dizdarem <meli.dizdarevic@gmail.com>
2019-12-03 20:24:17 +0100

    test

---
+++ Android_App/app/src/main/java/at/htl_villach/android_app/MainActivity.java
@@ -0,0 +1,15 @@
+package at.htl_villach.android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello");
+    }
+}
